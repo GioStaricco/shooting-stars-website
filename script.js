6f442920d552494fe1c2b5d54398c652b3a84dc8
GioStaricco <giostar071@gmail.com>
2026-07-02 18:17:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.site-nav');
+if (btn && nav) {
+  btn.addEventListener('click', () => {
+    const open = nav.classList.toggle('open');
+    btn.setAttribute('aria-expanded', String(open));
+  });
+}
